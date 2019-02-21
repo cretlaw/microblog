54005d3222e75354792aa5d4e3c6fc06acd1eb55
walter <walter@pop-os.localdomain>
2019-02-20 20:02:36 -0800

    added sqlite db

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 
 
 class User(db.Model):
+    # pylint: disable=E1101
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -14,6 +15,7 @@ class User(db.Model):
 
 
 class Post(db.Model):
+    # pylint: disable=E1101
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
