1383df46230d7f67908a92ce44684cf601514f30
Walter Carbajal <walterc@pop-os.localdomain>
2019-02-25 22:50:14 -0800

    added logging

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
@@ -6,12 +7,14 @@ from app import login
 
 
 class User(UserMixin, db.Model):
-    # pylint: disable=E1101
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -19,12 +22,15 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
     def __repr__(self):
         return '<user {}>'.format(self.username)
 
 
 class Post(db.Model):
-    # pylint: disable=E1101
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
